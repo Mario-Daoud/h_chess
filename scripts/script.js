d16e4e35ca513db59b3f392c7f977a8d4381641a
Mario Daoud <r0895582@student.vives.be>
2023-06-08 20:33:19 +0200

    add: move validation for pawn, knight, bishop

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,9 @@ const setup = () => {
   const playerDisplay = document.querySelector("#player");
   const infoDisplay = document.querySelector("#info-display");
 
+  let currentPlayerColor = "white";
+  playerDisplay.innerHTML = currentPlayerColor;
+
   let startPositionId;
   let currentElement;
 
@@ -24,10 +27,10 @@ const setup = () => {
       }
 
       if (i <= boardWidth * 2 - 1) {
-        square.firstChild.firstChild.classList.add("darkPiece");
+        square.firstChild.firstChild.classList.add("black");
       }
       if (i >= boardWidth * 6) {
-        square.firstChild.firstChild.classList.add("lightPiece");
+        square.firstChild.firstChild.classList.add("white");
       }
 
       board.append(square);
@@ -46,11 +49,145 @@ const setup = () => {
 
   dragDrop = (e) => {
     e.stopPropagation();
-    e.target.parentNode.append(currentElement);
-    e.target.remove();
+    const correctColor =
+      currentElement.firstChild.classList.contains(currentPlayerColor);
+    const opponentColor = currentPlayerColor === "white" ? "black" : "white";
+    const taken = e.target.classList.contains("piece");
+    const takenByOpponent =
+      e.target.firstChild?.classList.contains(opponentColor);
+    const valid = checkIfValid(e.target);
+
+    if (correctColor) {
+      if (takenByOpponent && valid) {
+        e.target.parentNode.append(currentElement);
+        e.target.remove();
+        changePlayer();
+        return;
+      }
+
+      if (taken && !takenByOpponent) {
+        infoDisplay.textContent = "you cannot go here";
+        setTimeout(() => {
+          infoDisplay.textContent = "";
+        }, 3000);
+        return;
+      }
+
+      if (valid) {
+        e.target.append(currentElement);
+        changePlayer();
+        return;
+      }
+    }
   };
 
-  document.querySelectorAll("#gameboard .square").forEach((square) => {
+  checkIfValid = (target) => {
+    const targetNumber =
+      Number(target.getAttribute("square-nr")) ||
+      Number(target.parentNode.getAttribute("square-nr"));
+    const piece = currentElement.id;
+    const startId = Number(startPositionId);
+
+    switch (piece) {
+      case "pawn":
+        const startRow = [];
+        for (let i = 8; i < 16; i++) {
+          startRow.push(i);
+        }
+
+        if (
+          (startRow.includes(startId) &&
+            startId + boardWidth * 2 === targetNumber) ||
+          (startRow.includes(startId) &&
+            startId - boardWidth * 2 === targetNumber) ||
+          startId + boardWidth === targetNumber ||
+          startId - boardWidth === targetNumber ||
+          (startId + boardWidth - 1 === targetNumber &&
+            document.querySelector(`[square-nr="${startId + boardWidth - 1}"]`)
+              .firstChild) ||
+          (startId + boardWidth + 1 === targetNumber &&
+            document.querySelector(`[square-nr="${startId + boardWidth + 1}"]`)
+              .firstChild)
+        ) {
+          return true;
+        }
+
+      case "knight":
+        if (
+          startId + boardWidth * 2 - 1 === targetNumber ||
+          startId + boardWidth * 2 + 1 === targetNumber ||
+          startId - boardWidth * 2 - 1 === targetNumber ||
+          startId - boardWidth * 2 + 1 === targetNumber ||
+          startId + boardWidth - 2 === targetNumber ||
+          startId + boardWidth + 2 === targetNumber ||
+          startId - boardWidth - 2 === targetNumber ||
+          startId - boardWidth + 2 === targetNumber
+        ) {
+          return true;
+        }
+
+        case "bishop": 
+        if (
+startId + boardWidth + 1 === targetNumber ||
+startId + boardWidth * 2 + 2 === targetNumber && !document.querySelector(`[square-nr="${startId + boardWidth + 1}"]`).firstChild ||
+startId + boardWidth * 3 + 3 === targetNumber && !document.querySelector(`[square-nr="${startId + boardWidth * 2 + 2}"]`).firstChild ||
+startId + boardWidth * 4 + 4 === targetNumber && !document.querySelector(`[square-nr="${startId + boardWidth * 3 + 3}"]`).firstChild ||
+startId + boardWidth * 5 + 5 === targetNumber && !document.querySelector(`[square-nr="${startId + boardWidth * 4 + 4}"]`).firstChild ||
+startId + boardWidth * 6 + 6 === targetNumber && !document.querySelector(`[square-nr="${startId + boardWidth * 5 + 5}"]`).firstChild ||
+startId + boardWidth * 7 + 7 === targetNumber && !document.querySelector(`[square-nr="${startId + boardWidth * 6 + 6}"]`).firstChild ||
+startId - boardWidth - 1 === targetNumber ||
+startId - boardWidth * 2 - 2 === targetNumber && !document.querySelector(`[square-nr="${startId - boardWidth - 1}"]`).firstChild ||
+startId - boardWidth * 3 - 3 === targetNumber && !document.querySelector(`[square-nr="${startId - boardWidth * 2 - 2}"]`).firstChild ||
+startId - boardWidth * 4 - 4 === targetNumber && !document.querySelector(`[square-nr="${startId - boardWidth * 3 - 3}"]`).firstChild ||
+startId - boardWidth * 5 - 5 === targetNumber && !document.querySelector(`[square-nr="${startId - boardWidth * 4 - 4}"]`).firstChild ||
+startId - boardWidth * 6 - 6 === targetNumber && !document.querySelector(`[square-nr="${startId - boardWidth * 5 - 5}"]`).firstChild ||
+startId - boardWidth * 7 - 7 === targetNumber && !document.querySelector(`[square-nr="${startId - boardWidth * 6 - 6}"]`).firstChild ||
+startId + boardWidth - 1 === targetNumber ||
+startId + boardWidth * 2 - 2 === targetNumber && !document.querySelector(`[square-nr="${startId + boardWidth - 1}"]`).firstChild ||
+startId + boardWidth * 3 - 3 === targetNumber && !document.querySelector(`[square-nr="${startId + boardWidth * 2 - 2}"]`).firstChild ||
+startId + boardWidth * 4 - 4 === targetNumber && !document.querySelector(`[square-nr="${startId + boardWidth * 3 - 3}"]`).firstChild ||
+startId + boardWidth * 5 - 5 === targetNumber && !document.querySelector(`[square-nr="${startId + boardWidth * 4 - 4}"]`).firstChild ||
+startId + boardWidth * 6 - 6 === targetNumber && !document.querySelector(`[square-nr="${startId + boardWidth * 5 - 5}"]`).firstChild ||
+startId + boardWidth * 7 - 7 === targetNumber && !document.querySelector(`[square-nr="${startId + boardWidth * 6 - 6}"]`).firstChild ||
+startId - boardWidth + 1 === targetNumber ||
+startId - boardWidth * 2 + 2 === targetNumber && !document.querySelector(`[square-nr="${startId - boardWidth + 1}"]`).firstChild ||
+startId - boardWidth * 3 + 3 === targetNumber && !document.querySelector(`[square-nr="${startId - boardWidth * 2 + 2}"]`).firstChild ||
+startId - boardWidth * 4 + 4 === targetNumber && !document.querySelector(`[square-nr="${startId - boardWidth * 3 + 3}"]`).firstChild ||
+startId - boardWidth * 5 + 5 === targetNumber && !document.querySelector(`[square-nr="${startId - boardWidth * 4 + 4}"]`).firstChild ||
+startId - boardWidth * 6 + 6 === targetNumber && !document.querySelector(`[square-nr="${startId - boardWidth * 5 + 5}"]`).firstChild ||
+startId - boardWidth * 7 + 7 === targetNumber && !document.querySelector(`[square-nr="${startId - boardWidth * 6 + 6}"]`).firstChild
+
+        ) {
+          return true;
+        }
+    }
+  };
+
+  changePlayer = () => {
+    if (currentPlayerColor === "white") {
+      revertIds();
+      currentPlayerColor = "black";
+      playerDisplay.innerHTML = currentPlayerColor;
+    } else {
+      reverseIds();
+      currentPlayerColor = "white";
+      playerDisplay.innerHTML = currentPlayerColor;
+    }
+  };
+
+  reverseIds = () => {
+    document.querySelectorAll(".square").forEach((square, i) => {
+      square.setAttribute("square-nr", boardWidth * boardWidth - 1 - i);
+    });
+  };
+
+  revertIds = () => {
+    document.querySelectorAll(".square").forEach((square, i) => {
+      square.setAttribute("square-nr", i);
+    });
+  };
+
+  document.querySelectorAll(".square").forEach((square) => {
     square.addEventListener("dragstart", dragStart);
     square.addEventListener("dragover", dragOver);
     square.addEventListener("drop", dragDrop);
